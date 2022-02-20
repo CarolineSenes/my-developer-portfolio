518ef3994c3b82fd91d56b3acdd9cd7c12eb42f7
Caroline SENES <caro67200@gmail.com>
2022-02-20 23:26:36 +0100

    Add footer component

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,33 @@
+<script>
+	import GithubIcon from './github-icon.svelte';
+	import TwitterIcon from './twitter-icon.svelte';
+	import LinkedinIcon from './linkedin-icon.svelte';
+	import MailIcon from './mail-icon.svelte'
+</script>
+
+<footer class="p-10 footer bg-primary text-primary-content footer-center">
+	<div class="grid grid-flow-col gap-4">
+		<a class="link link-hover" href="/projects">Portfolio</a>
+		<a class="link link-hover" href="/posts">Blog</a>
+		<a class="link link-hover" href="/about">A Propos</a>
+	</div>
+	<div>
+		<div class="grid grid-flow-col gap-4">
+			<a href="https://twitter.com/senes_caroline">
+				<TwitterIcon />
+			</a>
+			<a href="https://github.com/CarolineSenes">
+				<GithubIcon />
+			</a>
+			<a href="https://www.linkedin.com/in/carolinesenes/">
+				<LinkedinIcon />
+			</a>
+			<a href="mailto:senes.caroline@orange.fr">
+				<MailIcon />
+			</a>
+		</div>
+	</div>
+	<div>
+		<p>© {`${new Date().getFullYear()}`} - Made with Svelte</p>
+	</div>
+</footer>
