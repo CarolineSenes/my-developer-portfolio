9761a2aef81c7bf686560322156e59addf00d0c5
Caroline SENES <caro67200@gmail.com>
2022-02-22 00:57:35 +0100

    Modif footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -5,29 +5,37 @@
 	import MailIcon from './mail-icon.svelte';
 </script>
 
-<footer class="p-10 footer bg-primary text-primary-content footer-center">
-	<div class="grid grid-flow-col gap-4">
+<footer class="p-10 footer bg-primary text-primary-content">
+	<div>
+		<p>© {`${new Date().getFullYear()}`} - Créé avec Svelte, GraphCMS, Tailwind</p>
+		<p>Mentions légales</p>
+	</div>
+	<div class="gap-4">
+		<span class="footer-title">Plan</span>
 		<a class="link link-hover" href="/projects">Portfolio</a>
 		<a class="link link-hover" href="/posts">Blog</a>
 		<a class="link link-hover" href="/about">A Propos</a>
 	</div>
-	<div>
-		<div class="grid grid-flow-col gap-4">
-			<a href="https://twitter.com/senes_caroline">
-				<TwitterIcon />
-			</a>
-			<a href="https://github.com/CarolineSenes">
-				<GithubIcon />
-			</a>
-			<a href="https://www.linkedin.com/in/carolinesenes/">
-				<LinkedinIcon />
-			</a>
-			<a href="mailto:senes.caroline@orange.fr">
-				<MailIcon />
-			</a>
-		</div>
+	<div class="flex flex-col gap-4">
+		<span class="footer-title">Réseaux sociaux</span>
+		<a href="https://twitter.com/senes_caroline" class="link link-hover flex items-center gap-4">
+			<TwitterIcon />
+			<p>Twitter</p>
+		</a>
+		<a href="https://github.com/CarolineSenes" class="link link-hover flex items-center gap-4">
+			<GithubIcon />
+			<p>GitHub</p>
+		</a>
+		<a href="https://www.linkedin.com/in/carolinesenes/" class="link link-hover flex items-center gap-4">
+			<LinkedinIcon />
+			<p>LinkedIn</p>
+		</a>
 	</div>
 	<div>
-		<p>© {`${new Date().getFullYear()}`} - Made with Svelte, GraphCMS, Tailwind</p>
+		<span class="footer-title">Contact</span>
+		<a href="mailto:senes.caroline@orange.fr" class="link link-hover flex items-center gap-4">
+			<MailIcon />
+			<p>E-Mail</p>
+		</a>
 	</div>
 </footer>
