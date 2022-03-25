44c8c2742abd2fee00d4a6959d48b753f834733f
SENES <caro67200@gmail.com>
2022-03-25 14:02:16 +0100

    update Navbar style

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -4,7 +4,10 @@
 
 <nav class="navbar mb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10">
 	<div class="flex-1 px-2 mx-2">
-		<a class="text-lg font-bold" href="/">Caroline SENES</a>
+		<a class="text-lg font-bold navbar-name" href="/">
+			<span class="grey-color" aria-hidden="true"> &lt; </span>
+			Caroline SENES 
+			<span class="grey-color" aria-hidden="true">/&gt;</span></a>
 	</div>
 
 	<div class="dropdown dropdown-left lg:hidden">
@@ -13,7 +16,7 @@
 			tabindex="0"
 			class="bg-neutral rounded-box shadow text-neutral-content p-2 w-52 menu dropdown-content "
 		>
-			<a class="btn btn-ghost btn-sm rounded-btn" href="/projects"> Portfolio </a>
+			<a class="btn btn-ghost btn-sm rounded-btn" href="/projects"> Projets </a>
 			<a class="btn btn-ghost btn-sm rounded-btn" href="/posts"> Blog </a>
 			<a class="btn btn-ghost btn-sm rounded-btn" href="/about"> About </a>
 		</ul>
@@ -21,7 +24,7 @@
 
 	<div class="flex-none hidden px-2 mx-2 lg:flex">
 		<div class="flex items-center">
-			<a class="btn btn-ghost btn-sm rounded-btn" href="/projects"> Portfolio </a>
+			<a class="btn btn-ghost btn-sm rounded-btn" href="/projects"> Projets </a>
 			<a class="btn btn-ghost btn-sm rounded-btn" href="/posts">Blog</a>
 			<a class="btn btn-ghost btn-sm rounded-btn" href="/about">A Propos</a>
 			<div class="px-4">
